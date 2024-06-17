a4168055d9c03652eff462585002ed40ac1f8232
YAMAMOTO Masaya <pandax381@gmail.com>
2024-06-18 08:02:39 +0900

    Skeleton of step15

--- net.c
+++ net.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "arp.h"
 #include "icmp.h"
@@ -14,6 +15,13 @@ struct net_protocol {
   struct net_protocol *next;
   uint16_t type;
   net_protocol_handler_t handler;
+};
+
+struct net_protocol_queue_entry {
+  struct queue_entry entry;
+  struct net_device *dev;
+  size_t len;
+  /* data bytes exists after this structure. */
 };
 
 /*
@@ -162,7 +170,11 @@ int net_protocol_register(uint16_t type, net_protocol_handler_t handler) {
   return 0;
 }
 
-// TODO: ネットワークデバイスからのinputは割り込みで制御する、あとで実装
+static struct net_protocol_queue_entry *net_protocol_queue_push(struct net_protocol *proto, const uint8_t *data,
+                                                                size_t len, struct net_device *dev) {}
+
+static struct net_protocol_queue_entry *net_protocol_queue_pop(struct net_protocol *proto) {}
+
 int net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev) {
   struct net_protocol *proto;
 
@@ -178,6 +190,12 @@ int net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device 
   /* unsupported protocol */
   return 0;
 }
+
+void net_softirq_handler(unsigned int irq, void *arg) {}
+
+#include "arp.h"
+#include "icmp.h"
+#include "ip.h"
 
 int net_init(void) {
   infof("initialize...");
