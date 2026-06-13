978fd57965c934940d5e6a5a8ecbeb31d4e1c0a6
isseii10 <t1l.g2.o11g@gmail.com>
2026-06-13 13:04:51 +0000

    Completion of step7

--- net.c
+++ net.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include "icmp.h"
 #include "ip.h"
 #include "platform.h"
 
@@ -185,6 +186,10 @@ int net_init(void) {
   }
   if (ip_init() == -1) {
     errorf("ip_init() failure");
+    return -1;
+  }
+  if (icmp_init() == -1) {
+    errorf("icmp_init() failure");
     return -1;
   }
   infof("success");
