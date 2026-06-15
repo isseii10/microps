a874c6faed7b38d9a88ae4254cc1c07e76bc8805
isseii10 <t1l.g2.o11g@gmail.com>
2026-06-15 00:06:21 +0000

    Completion of step11

--- platform/linux/platform.c
+++ platform/linux/platform.c
@@ -4,18 +4,32 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "intr.h"
 #include "platform.h"
 
 #include "util.h"
 
 int platform_init(void) {
   srandom(time(NULL));
+  if (intr_init() == -1) {
+    return -1;
+  }
   return 0;
 }
 
-int platform_run(void) { return 0; }
+int platform_run(void) {
+  if (intr_run() == -1) {
+    return -1;
+  }
+  return 0;
+}
 
-int platform_shutdown(void) { return 0; }
+int platform_shutdown(void) {
+  if (intr_shutdown() == -1) {
+    return -1;
+  }
+  return 0;
+}
 
 /*
  * Memory
